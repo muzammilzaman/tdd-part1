f0aad21503e238f6a324678b3ccb7758f7cfea73
muzammilzaman <muzammilzaman>
2015-01-02 18:03:30 +0500

    Chapter 8

--- src/MoneyPackage/MoneyTest.java
+++ src/MoneyPackage/MoneyTest.java
@@ -10,14 +10,17 @@ import static org.junit.Assert.assertTrue;
 public class MoneyTest {
 	@Test
 	
-	public void testFrancMultiplication() {
-		/*Franc five= new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));*/
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+		} 
+	public void testEquality() {
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertFalse(new Franc(5).equals(Money.dollar(5)));
 		}
 
 }
