cd7b7b7c67a9ccbc984e31135c2e84ca73532711
muzammilzaman <muzammilzaman>
2015-01-02 18:14:24 +0500

    Chapter 8

--- src/MoneyPackage/Money.java
+++ src/MoneyPackage/Money.java
@@ -4,9 +4,14 @@ public abstract class Money {
 	protected int amount;
 	 
 	abstract Money times(int multiplier);
+	
 	static Money dollar(int amount) {
 		return new Dollar(amount);
 		}
+	static Money franc(int amount) {
+		return new Franc(amount);
+		}
+	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount
